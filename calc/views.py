c20479f43918a8543c9256b4a36352ea51bc25e2
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-04 17:53:33 +0100

    Improve algorithm

--- calc/views.py
+++ calc/views.py
@@ -7,16 +7,29 @@ from os import getenv
 # Create your views here.
 @api_view(['POST', ])
 def Calc(request):
+	# send request to openai
 	data = request.data
-	print(data)
 	prompt = data['operation_type'] + ", return just the result"
 	engine = 'text-davinci-002'
 	openai.api_key = getenv('API_KEY')
 	response = openai.Completion.create(engine=engine, prompt=prompt, temperature=1)
-	print(response)
 	print(data)
+
+	try:
+		result = int(response['choices'][0]['text'].strip('\n'))
+	except Exception:
+		prompt = f"perform {data['operation_type']} on {data['x']} and {data['y']}, return just the result as a number"
+		response = openai.Completion.create(engine=engine, prompt=prompt, temperature=1)
+		try:
+			result = int(response['choices'][0]['text'].strip('\n'))
+		except Exception:
+			prompt = f"{data['operation_type']} {data['x']} and {data['y']}, return just the result as a number"
+			response = openai.Completion.create(engine=engine, prompt=prompt, temperature=1)
+			result = int(response['choices'][0]['text'].strip('\n'))
+
+
 	return JsonResponse({
 		'slackUsername': 'Lase',
-		'result': int(response['choices'][0]['text'].strip('\n')),
+		'result': result,
 		'operation_type': data['operation_type'],
 	})
